04cc8c9c4a0fff37161cc2fd141ee630880ccea7
emejotaw <matheusjosexxx@gmail.com>
2023-12-23 23:40:18 -0300

    feat: add controllers and services

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"encoding/json"
-	"os"
+	"log"
 
 	"github.com/emejotaw/product-api/config"
+	"github.com/emejotaw/product-api/internal/entity"
+	"github.com/emejotaw/product-api/internal/routes"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
 
-	config := config.New(".")
-	encoder := json.NewEncoder(os.Stdout)
-	encoder.Encode(config)
+	_ = config.New(".")
+	dsn := "database.db"
+	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Printf("could not establish database connection, error: %v", err)
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+	router := routes.NewRouter()
+	router.Run(db)
 }
